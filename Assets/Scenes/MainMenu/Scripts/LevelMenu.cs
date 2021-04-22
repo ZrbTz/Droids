992abde66311f1c36cb1c067a120bc59e4398a95
ZrbTz <thomasmadeo@outlook.com>
2021-04-22 17:15:28 +0200

    Added MainMenu Scene

---
+++ Assets/Scenes/MainMenu/Scripts/LevelMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour{
+
+    public void StartLevelOne(){
+
+        SceneManager.LoadScene(1); //For the moment i just load level1
+    }
+}
