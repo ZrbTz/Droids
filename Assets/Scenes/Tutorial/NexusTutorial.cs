d98d6919c058313c958c9219bf247cc64bf84164
Bylom <giuseppe.virzi6@gmail.com>
2021-06-24 02:12:00 +0200

    add: try again wave on tutorial level

--- Assets/Scenes/Tutorial/NexusTutorial.cs
+++ Assets/Scenes/Tutorial/NexusTutorial.cs
@@ -1,25 +1,23 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class NexusTutorial : Unit{
-
-//    protected override void Die() {
-//        GameManager gm = GameManager.Instance;
-//        gm.resetHorde();
-//        health = 5;
-//        dead = false;
-//    }
-//}
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NexusTutorial : Unit {
-    protected override void Die() {
+public class NexusTutorial : Unit
+{
+    private GameUI gameUI;
+
+    void Awake()
+    {
+        gameUI = FindObjectOfType<GameUI>();
+    }
+
+    protected override void Die()
+    {
         GameManager gm = GameManager.Instance;
         health = 5;
         dead = false;
         gm.resetHorde();
+
+        gameUI.UpdateNotification("You lost, try to fight this wave again!");
     }
 }
