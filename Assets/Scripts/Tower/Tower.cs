f9921ef3cd677caf396a64407140eadf3932bf23
ZrbTz <thomasmadeo@outlook.com>
2021-07-13 22:31:47 +0200

    Deleted old scripts and prefabs (2)

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Tower : Unit {
     public enum TowerState {
@@ -9,27 +10,25 @@ public class Tower : Unit {
         Attacking
     }
 
-    public float range = 7.5f;
+    public float range = 10f;
     public float damage = 50f;
     public float fireRate = 1f;
     public float cannonAngularSpeed = 360f;
-    public Transform cannonSphere;
+    public Transform cannonBase;
+    public Transform cannonPivot;
     public Transform cannonMouth;
     public Projectile projectilePrefab;
-
+    public AudioClip shotSound;
+    public AudioSource audioSource;
     private Unit target = null;
     public TowerState state;
-    private int buildingLayerMask;
+    public LayerMask lineOfFireLayerMask;
+    [SerializeField] private LayerMask enemyLayerMask;
     private float attackTime = Mathf.NegativeInfinity;
-    private Transform nexus;
-    private float cannonLength;
 
     protected override void Start() {
         base.Start();
-        buildingLayerMask = 1 << LayerMask.NameToLayer("Building");
-        nexus = GameManager.Instance.nexus.transform;
         state = TowerState.Idle;
-        cannonLength = Vector3.Distance(cannonMouth.position, cannonSphere.position);
     }
 
     protected override void Update() {
@@ -38,7 +37,7 @@ public class Tower : Unit {
             return;
         switch (state) {
             case TowerState.Idle:
-                if (GetTarget()) 
+                if (GetTarget())
                     state = TowerState.Rotating;
                 break;
             case TowerState.Rotating:
@@ -49,7 +48,6 @@ public class Tower : Unit {
                 RotateWeapon();
                 if (CannonTargetAngle() <= 5f)
                     state = TowerState.Attacking;
-                
                 break;
             case TowerState.Attacking:
                 if (!CheckTarget()) {
@@ -73,45 +71,85 @@ public class Tower : Unit {
     private bool GetTarget() {
         bool found = false;
         float minDistance = Mathf.Infinity;
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
+        int maxPassedPath = -100000;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, range, enemyLayerMask);
         foreach (Collider hitCollider in hitColliders) {
-            if (!(hitCollider.transform.root.TryGetComponent<Unit>(out Unit unit) && !unit.dead && unit.enemy))
+            if (!(hitCollider.transform.root.TryGetComponent(out Enemy enemy) && !enemy.dead && enemy.enemy))
                 continue;
-            if (!LineOfFire(unit))
+            if (!LineOfFire(enemy))
                 continue;
-            float distance = Vector3.Distance(unit.transform.position, nexus.position);
-            if (distance <= minDistance) {
-                target = unit;
+            float distance = enemy.GetPathRemainingDistance();
+            //Debug.Log(distance);
+            //if (!enemy.marching) { distance = range; }
+            if (enemy.passedPath > maxPassedPath || (enemy.passedPath == maxPassedPath && distance <= minDistance)) {
+                target = enemy;
                 found = true;
                 minDistance = distance;
+                maxPassedPath = enemy.passedPath;
             }
         }
+        /*if (found)
+        {
+            Debug.Log(maxPassedPath);
+            Debug.Log(minDistance);
+        }*/
         return found;
     }
 
     private void RotateWeapon() {
-        float delta = cannonAngularSpeed * Time.deltaTime;
-        Vector3 lookRotation = Quaternion.LookRotation(target.body.position - cannonSphere.position).eulerAngles;
-        cannonSphere.rotation = Quaternion.RotateTowards(cannonSphere.rotation, Quaternion.Euler(lookRotation), delta);
+        float maxDelta = cannonAngularSpeed * Time.deltaTime;
+        Vector3 lookRotation = Quaternion.LookRotation(target.body.position - cannonPivot.position).eulerAngles;
+        lookRotation = WorldRotationToLocal(lookRotation);
+        Vector3 baseTargetRotation = new Vector3(0f, lookRotation.y, 0f);
+        Vector3 pivotTargetRotation = new Vector3(lookRotation.x, 0f, lookRotation.z);
+        cannonBase.localRotation = Quaternion.RotateTowards(cannonBase.localRotation, Quaternion.Euler(baseTargetRotation), maxDelta);
+        cannonPivot.localRotation = Quaternion.RotateTowards(cannonPivot.localRotation, Quaternion.Euler(pivotTargetRotation), maxDelta);
+    }
+
+    private Vector3 WorldRotationToLocal(Vector3 angles) {
+        Transform child = transform.GetChild(0);
+        Quaternion temp = child.rotation;
+        child.eulerAngles = angles;
+        angles = child.localEulerAngles;
+        child.rotation = temp;
+        return angles;
     }
 
     private float CannonTargetAngle() {
-        return Quaternion.Angle(cannonSphere.rotation, Quaternion.LookRotation(target.body.position - cannonSphere.position));
+        return Quaternion.Angle(cannonPivot.rotation, Quaternion.LookRotation(target.body.position - cannonPivot.position));
     }
 
     private bool LineOfFire(Unit unit) {
-        float distance = Vector3.Distance(cannonSphere.position, unit.body.position);
-        Ray ray = new Ray(cannonSphere.position, unit.body.position - cannonSphere.position);
-        return !Physics.Raycast(ray, out RaycastHit hitInfo, distance, buildingLayerMask);
+        float distance = Vector3.Distance(cannonPivot.position, unit.body.position);
+        Ray ray = new Ray(cannonPivot.position, unit.body.position - cannonPivot.position);
+        return !Physics.Raycast(ray, out RaycastHit hitInfo, distance, lineOfFireLayerMask);
     }
 
     private void FireCannon() {
         Projectile projectile = Instantiate(projectilePrefab, cannonMouth.position, cannonMouth.rotation);
         projectile.target = target;
         projectile.damage = damage;
+        audioSource.PlayOneShot(shotSound);
     }
 
     private bool CheckTarget() {
-        return !target.dead && Distance(target.gameObject) <= range && LineOfFire(target);
+        if (target == null) return false;
+        return !target.dead && Vector3.Distance(transform.position, target.transform.position) <= range && LineOfFire(target);
     }
 }
+
+//public static class ExtensionMethods {
+//    public static float GetPathRemainingDistance(this NavMeshAgent navMeshAgent) {
+//        if (navMeshAgent.pathPending || navMeshAgent.pathStatus == NavMeshPathStatus.PathInvalid ||
+//                navMeshAgent.pathStatus == NavMeshPathStatus.PathInvalid ||
+//                navMeshAgent.path.corners.Length == 0)
+//            return 10000f;
+
+//        float distance = 0.0f;
+//        for (int i = 0; i < navMeshAgent.path.corners.Length - 1; ++i) {
+//            distance += Vector3.Distance(navMeshAgent.path.corners[i], navMeshAgent.path.corners[i + 1]);
+//        }
+
+//        return distance;
+//    }
+//}
