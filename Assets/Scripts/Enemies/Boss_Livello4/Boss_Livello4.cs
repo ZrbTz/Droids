28dfd497ab8dab64daf30b2bce73d8dc093ac2ca
SirLire <62445994+SirLire@users.noreply.github.com>
2021-09-30 23:09:06 +0200

    Fix: può essere danneggiato solo quando appropriato

--- Assets/Scripts/Enemies/Boss_Livello4/Boss_Livello4.cs
+++ Assets/Scripts/Enemies/Boss_Livello4/Boss_Livello4.cs
@@ -134,6 +134,6 @@ public class Boss_Livello4 : Enemy
                 StartCoroutine(sparaProiettili);
                 break;
         }
-        pdb.EnableInteraction();
+        pdb.DisableInteraction();
     }
 }
