f1caa47e52242e01e810f2e1ab823d80bc30017f
ZrbTz <thomasmadeo@outlook.com>
2023-06-10 18:54:42 +0200

    Clean up 5

--- Assets/PlayerManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerManager : Unit{
-    protected override void Start() {
-        base.Start();
-    }
-}
